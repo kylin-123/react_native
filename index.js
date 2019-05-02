ca974184a9c7a3a580f529c529ec02846944650c
kylin <1969044316@qq.com>
2019-05-02 14:34:11 +0800

    code push

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
-// import RootScene from './src/RootScene'
 
-// AppRegistry.registerComponent('AwesomeProjectIOS', () => RootScene)
 AppRegistry.registerComponent('AwesomeProjectIOS', () => App)
